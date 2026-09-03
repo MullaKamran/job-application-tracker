96957883d1f46c048790c348a3a2a5203cccd94a
Mulla Md Kamran <143728592+MullaKamran@users.noreply.github.com>
2026-09-03 23:56:20 +0530

    Day 14: Job Tracker CLI menu - full CRUD wired together

---
+++ main.py
@@ -0,0 +1,39 @@
+from crud import add_application,view_applications,update_application,delete_application
+def main():
+    while True:
+        print("\n JOB APPLICATION TRACKER")
+        print("1.Add a new job application")
+        print("2.View all job applications")
+        print("3.Update a job application")
+        print("4.Delete a job application")
+        print("5.Exit")
+        
+        choice=input("Enter your choice: ")
+        if choice=="1":
+            company=input("Enter company name: ")
+            role=input("Enter role: ")
+            status=input("Enter status: ")
+            date_applied=input("Enter date applied (YYYY-MM-DD): ")
+            notes=input("Enter notes: ")
+            add_application(company,role,status,date_applied,notes)
+        
+        elif choice=="2":
+            applications=view_applications()
+            for app in applications:
+                print(app)    
+        elif choice=="3":
+               app_id=int(input("Enter application ID to  update:"))
+               new_status=input("Enter new status: ")
+               update_application(app_id,new_status)
+               
+        elif choice=="4":
+                app_id=int(input("Enter applicarion ID to delete:"))
+                delete_application(app_id)
+                
+        elif choice=="5":
+            print("goodbye!")
+        
+            break
+        else:
+            print("You selected option", choice)
+main()            
